1622879cf2aba742cd1f0856b5ea3e27cb27b014
bilgekosee <bilge.koosee@gmail.com>
2024-11-07 20:36:18 +0300

    css sayfasına yeni eklemeler yapıldı

--- src/Pages/CSS.js
+++ src/Pages/CSS.js
@@ -4,7 +4,7 @@ function CSS() {
   return (
     <div className="css-container">
       <img
-        src="https://files.oaiusercontent.com/file-J30i7AbtxjmiJ3LLgr7goff7?se=2024-11-05T18%3A06%3A49Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D85de671f-0e21-4c1d-a4f5-45753d02f6d1.webp&sig=%2B6NCekw39OmpaTY1a2ghKDm2xSis2A7lhiZhdC%2BiDuk%3D"
+        src="https://images.unsplash.com/photo-1523437113738-bbd3cc89fb19?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         width="500px"
         height="300px"
       ></img>
@@ -472,7 +472,424 @@ Lorem ipsum dolor.... <p class="ex4">HELLO WORLD!</p> Vestibulum volutpat tellus
               </div>
             </ul>
           </li>
+          <li>
+            <strong>Flexbox ve Grid Layout Derinlemesine:</strong>:
+            <ul style={{ marginLeft: "15px" }}>
+              <li>
+                <strong>Flexbox Temelleri</strong>: Ana eksen, çapraz eksen
+                tanımları; justify-content, align-items, flex-direction
+                kullanımı.
+              </li>
+              <li>
+                <strong>Grid Layout Temelleri</strong>: Satır
+                (grid-template-rows) ve sütun (grid-template-columns) oluşturma,
+                alanları birleştirme.
+              </li>
+              <div className="example-css-grid">
+                <div class="flex-container">
+                  <div class="flex-item">1</div>
+                  <div class="flex-item">2</div>
+                  <div class="flex-item">3</div>
+                </div>
+
+                <div>
+                  <pre>
+                    <code>
+                      {`<div class="flex-container">
+  <div class="flex-item">1</div>
+  <div class="flex-item">2</div>
+  <div class="flex-item">3</div>
+</div>
+
+
+.flex-container {
+  display: flex;
+  flex-direction: row; /* Ana eksen yatay */
+  justify-content: center; /* Ana eksende ortala */
+  align-items: center; /* Çapraz eksende ortala */
+  height: 100vh;
+}
+
+.flex-item {
+  padding: 20px;
+  background-color: #4CAF50;
+  color: white;
+  margin: 10px;
+}
+`}
+                    </code>
+                  </pre>
+                </div>
+              </div>
+              <div className="example-css-grid">
+                <div class="grid-container">
+                  <div class="grid-item">A</div>
+                  <div class="grid-item">B</div>
+                  <div class="grid-item">C</div>
+                  <div class="grid-item">D</div>
+                </div>
+
+                <div>
+                  <pre>
+                    <code>
+                      {`<div class="grid-container">
+  <div class="grid-item">A</div>
+  <div class="grid-item">B</div>
+  <div class="grid-item">C</div>
+  <div class="grid-item">D</div>
+</div>
+
+
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 2fr; /* İlk sütun dar, ikincisi geniş */
+  grid-template-rows: 100px 200px; /* İlk satır kısa, ikincisi uzun */
+  gap: 10px;
+  height: 100vh;
+}
+
+.grid-item {
+  background-color: #FF5733;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+/* C'yi genişletme */
+.grid-item:nth-child(3) {
+  grid-column: 1 / span 2; /* İki sütunu kapla */
+}
+
+
+`}
+                    </code>
+                  </pre>
+                </div>
+              </div>
+            </ul>
+          </li>
         </ul>
+      </div>
+      <div className="content-css">
+        <h2>CSS Animasyon ve Geçişler</h2>
+        <li>
+          <strong>Transition</strong>:transition: all 0.3s ease; ile yumuşak
+          geçişler sağlama.
+        </li>
+        <div className="example-css">
+          <button className="transition">Transition</button>
+          <pre>
+            <code>
+              {`<button className="transition">Transition</button>
+
+.transition {
+  width: 100px;
+  height: 100px;
+  background: rgb(133, 25, 25);
+  transition: width 2s;
+}
+
+.transition:hover {
+  width: 300px;
+}
+
+`}
+            </code>
+          </pre>
+        </div>
+        <li>
+          <strong>@keyframes</strong>: @keyframes kuralı animasyon kodunu
+          belirtir. Animasyon, bir CSS stilinden diğerine kademeli olarak
+          geçilerek oluşturulur.
+        </li>
+
+        <li>
+          <strong>Animasyon Süresi ve Zamanlama Fonksiyonu</strong>:
+          <li style={{ marginLeft: "15px" }}>
+            animation-duration, animation-delay, animation-timing-function ile
+            animasyonun hızını ve akışını belirleme.
+          </li>
+        </li>
+        <div className="example-css" style={{ height: "500px" }}>
+          <button className="keyframes">@keyframes</button>
+          <pre>
+            <code>
+              {`<button className="keyframes">@keyframes</button>
+
+.keyframes {
+  width: 100px;
+  height: 100px;
+  background: red;
+  position: relative;
+  animation: mymove 5s infinite;
+}
+
+@keyframes mymove {
+  0% {
+    top: 0px;
+    background: red;
+    width: 100px;
+  }
+  100% {
+    top: 200px;
+    background: yellow;
+    width: 300px;
+  }
+}
+
+`}
+            </code>
+          </pre>
+        </div>
+        <div className="exercise">
+          <ol>
+            <p>
+              <strong>ALIŞTIRMA</strong>
+            </p>
+            <li value="1">
+              CSS animasyonları araştırarak çeşitli @keyframes örnekleri
+              hazırlayın. Geçişlerin farklı zamanlamalarını deneyin.
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div className="content-css">
+        <h2>Responsive Design ve Media Queries</h2>
+        <li>
+          <strong>Media Queries</strong>:Ekran boyutlarına göre farklı stiller
+          uygulanmasını sağlar.
+        </li>
+        <li>
+          <strong>Mobil Uyumluluk </strong>:
+          <ul style={{ marginLeft: "30px" }}>
+            <li>
+              vw, vh, rem gibi birimlerle ekran genişliği ve yüksekliğine uyumlu
+              tasarımlar yapılır.
+            </li>
+            <li>
+              Responsive Layout ile flex ve Grid kullanarak esnek düzenler
+              oluşturulabilir.
+            </li>
+          </ul>
+        </li>
+        <div className="example-css-responsive">
+          <div>
+            <div class="header">
+              <h1>Chania</h1>
+            </div>
+
+            <div class="row">
+              <div class="col-3 menu">
+                <ul>
+                  <li>The Flight</li>
+                  <li>The City</li>
+                  <li>The Island</li>
+                  <li>The Food</li>
+                </ul>
+              </div>
+
+              <div class="col-6">
+                <h1>The City</h1>
+                <p>
+                  Chania is the capital of the Chania region on the island of
+                  Crete. The city can be divided in two parts, the old town and
+                  the modern city.
+                </p>
+              </div>
+
+              <div class="col-3 right">
+                <div class="aside">
+                  <h2>What?</h2>
+                  <p>Chania is a city on the island of Crete.</p>
+                  <h2>Where?</h2>
+                  <p>Crete is a Greek island in the Mediterranean Sea.</p>
+                  <h2>How?</h2>
+                  <p>You can reach Chania airport from all over Europe.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="footer">
+              <p>
+                Resize the browser window to see how the content respond to the
+                resizing.
+              </p>
+            </div>
+          </div>
+          <pre>
+            <code>
+              {`<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: block;
+}
+
+[class*="col-"] {
+  float: left;
+  padding: 15px;
+}
+
+html {
+  font-family: "Lucida Sans", sans-serif;
+}
+
+.header {
+  background-color: #9933cc;
+  color: #ffffff;
+  padding: 15px;
+}
+
+.menu ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.menu li {
+  padding: 8px;
+  margin-bottom: 7px;
+  background-color: #33b5e5;
+  color: #ffffff;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+}
+
+.menu li:hover {
+  background-color: #0099cc;
+}
+
+.aside {
+  background-color: #33b5e5;
+  padding: 15px;
+  color: #ffffff;
+  text-align: center;
+  font-size: 14px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+}
+
+.footer {
+  background-color: #0099cc;
+  color: #ffffff;
+  text-align: center;
+  font-size: 12px;
+  padding: 15px;
+}
+
+/* For desktop: */
+.col-1 {width: 8.33%;}
+.col-2 {width: 16.66%;}
+.col-3 {width: 25%;}
+.col-4 {width: 33.33%;}
+.col-5 {width: 41.66%;}
+.col-6 {width: 50%;}
+.col-7 {width: 58.33%;}
+.col-8 {width: 66.66%;}
+.col-9 {width: 75%;}
+.col-10 {width: 83.33%;}
+.col-11 {width: 91.66%;}
+.col-12 {width: 100%;}
+
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
+  [class*="col-"] {
+    width: 100%;
+  }
+}
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>Chania</h1>
+</div>
+
+<div class="row">
+  <div class="col-3 menu">
+    <ul>
+    <li>The Flight</li>
+    <li>The City</li>
+    <li>The Island</li>
+    <li>The Food</li>
+    </ul>
+  </div>
+
+  <div class="col-6">
+    <h1>The City</h1>
+    <p>Chania is the capital of the Chania region on the island of Crete. The city can be divided in two parts, the old town and the modern city.</p>
+  </div>
+
+  <div class="col-3 right">
+    <div class="aside">
+      <h2>What?</h2>
+      <p>Chania is a city on the island of Crete.</p>
+      <h2>Where?</h2>
+      <p>Crete is a Greek island in the Mediterranean Sea.</p>
+      <h2>How?</h2>
+      <p>You can reach Chania airport from all over Europe.</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Resize the browser window to see how the content respond to the resizing.</p>
+</div>
+
+</body>
+</html>
+
+
+
+
+`}
+            </code>
+          </pre>
+        </div>
+
+        <div className="exercise">
+          <ol>
+            <p>
+              <strong>ALIŞTIRMA</strong>
+            </p>
+            <li value="1">
+              Farklı cihaz boyutları için bir medya sorgusu seti hazırlayın. Hem
+              Flexbox hem Grid ile responsive bir layout oluşturun.
+            </li>
+          </ol>
+        </div>
+      </div>
+      <div className="content-css">
+        <h2>CSS Ön İşlemciler ve Araçlar</h2>
+        <li>
+          <strong>Sass ve LESS’e Giriş</strong>: Sass ve LESS, CSS’i daha
+          modüler ve düzenli hale getiren ön işlemcilerdir.
+        </li>
+        <div className="example-css">
+          <pre>
+            <code>{`$primary-color: #3498db;
+
+body {
+    color: $primary-color;
+}
+`}</code>
+          </pre>
+        </div>
+        <li>
+          <strong>Değişkenler</strong>: Tek bir renk veya stil değeri bir
+          değişken olarak tanımlanabilir.
+        </li>
+        <li>
+          <strong>Mixins</strong>: Belirli stilleri tekrar kullanmak için
+          fonksiyon benzeri yapılardır.
+        </li>
       </div>
     </div>
   );
